2cc788986ad579bbbab0e8e60041ad0ad3ed7c72
larsfrommars <larsfrommars@gmail.com>
2015-08-06 09:47:43 +0200

    w.i.p.

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -4,6 +4,9 @@
   spriteConfig.w = 16;
   spriteConfig.h = 16;
 
-  var spriteEditor = new coolbox.SpriteEditor( 'canvas', spriteConfig );
+  var spriteEditor = new coolbox.SpriteEditor( 'editor', spriteConfig );
+
+  var spriteOverview = new coolbox.SpriteOverview( 'overview', spriteConfig );
+
 
 }())
